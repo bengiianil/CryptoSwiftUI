fe1e985ed5c50e95ba1bc20a9164bddda68d8ce1
Bengi Anıl <banil2@ford.com.tr>
2024-03-26 13:54:09 +0300

    Create project files

---
+++ CryptoSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CryptoSwiftUI
+//
+//  Created by Bengi Anıl on 26.03.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CryptoSwiftUI/CryptoSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoSwiftUIApp.swift
+//  CryptoSwiftUI
+//
+//  Created by Bengi Anıl on 26.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

89929fe712b6490bca467a35f193cdcba25b6792
Bengi Anıl <banil2@ford.com.tr>
2024-03-26 16:23:17 +0300

    Created project structure files and added color themes

---
+++ CryptoSwiftUI/Components/CircleButton.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButton.swift
+//  CryptoSwiftUI
+//
+//  Created by Bengi Anıl on 26.03.2024.
+//
+
+import SwiftUI
+
+struct CircleButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButton()
+}
